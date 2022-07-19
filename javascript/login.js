f7ab2e69f66b727fb88def83cf32738f3df067b3
josemaria1988 <idontknow1988@gmail.com>
2022-07-18 22:26:23 -0300

    registro de usuario pronto, falta login y bajar de localstorage

--- javascript/login.js
+++ javascript/login.js
@@ -25,28 +25,30 @@ function registrarse() {
     let pass1 = document.getElementById("pass1");
     let pass2 = document.getElementById("pass2");
 
-    if (pass1.value != pass2.value) {
-        swal("La contraseña y su confirmación no coinciden")
+    if ( pass1.value.length == 0 || pass1.value != pass2.value ) {
+        Swal.fire({
+            icon: 'error',
+            text: "La contraseña y su confirmación no coinciden"})
     }else if (usuario.includes(email)){
-        swal({
+        Swal.fire({
             icon: ' error',
             title: 'Oops..',
             text: "El email ya está en uso"
         });
-    }else if((nombre.value ="") || (apellido.value ="") || (nickname.value ="") || (email.value ="")) {
-        swal({
+    }else if((nombre.value.length ==0) || (apellido.value.length ==0) || (nickname.value.length ==0) || (email.value.length ==0)) {
+        Swal.fire({
             icon:'error',
             text: "Debes ingresar todos los datos"
         });
     }else {
         usuario.push(new Usuario(nombre.value, apellido.value, nickname.value, email.value, pass1.value));
         localStorage.setItem('usuario', JSON.stringify(usuario));
-        swal({
-            position: 'top-end',
+        Swal.fire({
             icon: 'success',
-            title: 'Bienvenido!',
-            text: 'Código de descuento de Usuario 123987',
-            button: "Ok"
+            title: '<strong><u>Bienvenido!</u></strong>',
+            html: 'Ve a usar tu código y obtén <b>UN GRAN DESCUENTO</b>, ' +
+                  '<a href="index.html">Volver a Inicio</a>'
+            
           })
     }
 }
